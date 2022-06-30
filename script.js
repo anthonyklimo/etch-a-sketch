3596d37146a78f7e9d4786fc76509e0dba8278ca
Anthony <cooperkl317@gmail.com>
2022-06-29 18:33:44 -0700

    add function to change grid size. buttons now change grid size

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
 const gridContainer = document.querySelector('#gridContainer');
 
+/* buttons to choose size of grid*/
+const sizeButtons = document.querySelector('#sizeButtons');
+
+const smallButton = document.createElement('button');
+smallButton.classList.add('smallButton');
+smallButton.textContent = '16x16';
+
+const medButton = document.createElement('button');
+medButton.classList.add('smallButton');
+medButton.textContent = '32x32';
+
+const largeButton = document.createElement('button');
+largeButton.classList.add('largeButton');
+largeButton.textContent = '48x48';
+
+
+
+sizeButtons.appendChild(smallButton);
+sizeButtons.appendChild(medButton);
+sizeButtons.appendChild(largeButton);
+
+smallButton.addEventListener("click", function() {
+    unMakeGrid();
+    makeGrid(256);
+    changeGridSize(16);
+});
+
+medButton.addEventListener("click", function() {
+    unMakeGrid();
+    makeGrid(1024);
+    changeGridSize(32);
+});
+
+largeButton.addEventListener("click", function() {
+    unMakeGrid();
+    makeGrid(2304);
+    changeGridSize(48);
+});
+
+
+/*make grid of n boxes */
 function makeGrid(n) {
     for (let i = 0; i < n; i++) {
         const box = document.createElement('div');
@@ -13,6 +54,16 @@ function makeGrid(n) {
         }
 }
 
+function unMakeGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    
+}
 
+function changeGridSize(n) {
+    gridContainer.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+}
 
 makeGrid(256);
+

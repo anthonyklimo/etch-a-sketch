a0c5745ba955d3f3fe7170afe66c3ca207cbcabd
Anthony <cooperkl317@gmail.com>
2022-07-13 19:46:10 -0700

    add functional buttons for white and rainbow color

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
+let gridSize = 256;
+
 const gridContainer = document.querySelector('#gridContainer');
 
 /* buttons to choose size of grid*/
 const sizeButtons = document.querySelector('#sizeButtons');
 
 const smallButton = document.createElement('button');
-smallButton.classList.add('smallButton');
-smallButton.textContent = '16x16';
-
 const medButton = document.createElement('button');
-medButton.classList.add('smallButton');
-medButton.textContent = '32x32';
-
 const largeButton = document.createElement('button');
+
+smallButton.classList.add('smallButton');
+medButton.classList.add('smallButton');
 largeButton.classList.add('largeButton');
-largeButton.textContent = '48x48';
 
-
+smallButton.textContent = '16x16';
+medButton.textContent = '32x32';
+largeButton.textContent = '64x64';
 
 sizeButtons.appendChild(smallButton);
 sizeButtons.appendChild(medButton);
@@ -23,37 +23,71 @@ sizeButtons.appendChild(largeButton);
 
 smallButton.addEventListener("click", function() {
     unMakeGrid();
-    makeGrid(256);
+    gridSize = 256;
+    makeGrid();
     changeGridSize(16);
-});
 
+});
 medButton.addEventListener("click", function() {
     unMakeGrid();
-    makeGrid(1024);
+    gridSize = 1024;
+    makeGrid();
     changeGridSize(32);
 });
-
 largeButton.addEventListener("click", function() {
     unMakeGrid();
-    makeGrid(2304);
-    changeGridSize(48);
+    gridSize = 4096;
+    makeGrid();
+    changeGridSize(64);
 });
+
+const drawButtons = document.querySelector('#drawButtons');
+const rainbowButton = document.createElement('button');
+const shaderButton = document.createElement('button');
+const whiteButton = document.createElement('button');
+
+whiteButton.textContent = 'White';
+rainbowButton.textContent = 'Rainbow';
+shaderButton.textContent = 'Shader';
+
+drawButtons.append(rainbowButton, shaderButton, whiteButton);
+
+
+whiteButton.addEventListener('click', function() {
+    gridContainer.addEventListener("mouseover", function(e) {
+        e.target.style.backgroundColor = 'white';
+    })
+});
+
+rainbowButton.addEventListener('click', function() {
+    gridContainer.addEventListener("mouseover", function(e) {
+        e.target.style.backgroundColor = rainbowInk();
+    })
+});
+
+const rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+function rainbowInk() {
+    let randInt = Math.floor(Math.random() * rainbowColors.length);
+    return rainbowColors[randInt];
+}
+
+
 
 
 /*make grid of n boxes */
-function makeGrid(n) {
-    for (let i = 0; i < n; i++) {
+function makeGrid() {
+    
+    for (let i = 0; i < gridSize; i++) {
         const box = document.createElement('div');
         box.classList.add('box');
-        box.style.backgroundColor ='blue';
-
+        box.style.backgroundColor ='black';
         gridContainer.appendChild(box);
         box.addEventListener("mouseover", function(e) {
             e.target.style.backgroundColor = 'white';
         });
         }
 }
-
+// removes current grid
 function unMakeGrid() {
     while (gridContainer.firstChild) {
         gridContainer.removeChild(gridContainer.firstChild);
@@ -61,9 +95,10 @@ function unMakeGrid() {
     
 }
 
+// changes grid size to grid of n boxes
 function changeGridSize(n) {
     gridContainer.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
 }
 
-makeGrid(256);
+makeGrid();
 
